008e979126c0855b5758f6facaf027ea9118e2a7
1 <1@05-PC>
2014-11-24 12:49:05 +0100

    ADD método consultar Todos de la clese negocio.java

---
+++ src/es/concesionario/controlador/ConsultarTodosServlert.java
@@ -0,0 +1,57 @@
+package es.concesionario.controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.concesionario.modelo.Coche;
+import es.concesionario.modelo.Negocio;
+
+
+
+/**
+ * Servlet implementation class ConsultarTodosServlert
+ */
+@WebServlet("/ConsultarTodos")
+public class ConsultarTodosServlert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConsultarTodosServlert() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+    
+    
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		   Negocio negocio= new Negocio();
+		  ArrayList<Coche> coches= negocio.consultarTodos();
+		  //meter el arraylist en el request....
+		  request.setAttribute("listado", coches);
+		  RequestDispatcher rd;
+		  rd=request.getRequestDispatcher("mostrarTodos.jsp");
+		  rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
