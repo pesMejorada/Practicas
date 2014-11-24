c131a39c04d1dc75a6a9496b47a7b0e87dcd1288
abm13h <abm13h@gmail.com>
2014-11-24 13:21:25 +0100

    cambiado paquete es.curso.controlador por es.concesionario.controlador

--- src/es/concesionario/controlador/ConsultarTodosServlet.java
+++ src/es/concesionario/controlador/ConsultarTodosServlet.java
@@ -13,19 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import es.concesionario.modelo.Coche;
 import es.concesionario.modelo.Negocio;
 
-
-
 /**
- * Servlet implementation class ConsultarTodosServlert
+ * Servlet implementation class consultarTodosServlet
  */
 @WebServlet("/ConsultarTodos")
-public class ConsultarTodosServlert extends HttpServlet {
+public class ConsultarTodosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ConsultarTodosServlert() {
+    public ConsultarTodosServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,25 +31,27 @@ public class ConsultarTodosServlert extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	
-    
-    
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		   Negocio negocio= new Negocio();
-		  ArrayList<Coche> coches= negocio.consultarTodos();
-		  //meter el arraylist en el request....
-		  request.setAttribute("listado", coches);
-		  RequestDispatcher rd;
-		  rd=request.getRequestDispatcher("mostrarTodos.jsp");
-		  rd.forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// invocar al Negocio
+		Negocio negocio=new Negocio();
+		// 1er escribo...
+		// ArrayList<Coche> coches=negocio.consultarTodos();
+		// me pide importar "Coche" de "es.concesionario.modelo"
+		ArrayList<Coche> coches=negocio.consultarTodos();
+		
+		// meter el arrayList en el request
+		request.setAttribute("listado", coches);
+		// redirigir al código jsp "mostrarTodos"
+		RequestDispatcher rd;
+		rd=request.getRequestDispatcher("mostrarTodos.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
 	}
 
 }
